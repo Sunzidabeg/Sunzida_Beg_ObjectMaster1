bf8d5cdb18c582b373b4fdc57966988185c394a2
Sunzidabeg <50387582+Sunzidabeg@users.noreply.github.com>
2019-06-30 18:41:25 -0500

    Add files via upload

---
+++ HumanTest.java
@@ -0,0 +1,13 @@
+package objectmaster1;
+
+
+public class HumanTest {
+    	public static void main(String[] args) {
+		Human h1 = new Human("Human One");
+		Human h2 = new Human("Human Two");
+		h1.attack(h2);
+		h1.display(h1);
+		h2.display(h2);
+	}
+}
+
